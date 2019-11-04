04798115334185b76ff1bbb46835bfdd67aee881
Hleb Rubanau <g.rubanau@gmail.com>
2019-11-04 21:42:16 +0300

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 set -o pipefail
 
 if [ -z "$WSID_PRIVATE_KEY" ]; then
-    export WSID_PRIVATE_KEY=$( /usr/bin/python3 /app/keygen.py | tee /dev/shm/debug.txt )
-
-    cat /dev/shm/debug.txt
+    export WSID_PRIVATE_KEY=$( /usr/bin/python3 /app/keygen.py )
 fi
 
 exec $*
