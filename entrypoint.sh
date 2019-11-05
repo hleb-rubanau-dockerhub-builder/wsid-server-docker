e147a4a9bf708bc4c7479c194ebcfbea8a352dc6
Hleb Rubanau <g.rubanau@gmail.com>
2019-11-05 07:15:12 +0300

    tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,15 @@
 set -e
 set -o pipefail
 
+function say() { echo "$*" >&2 ; }
+function die() { say "ERROR: $*" ; exit 1 ; }
+
 if [ -z "$WSID_PRIVATE_KEY" ]; then
+    say "Generating WSID_PRIVATE_KEY" 
     export WSID_PRIVATE_KEY=$( /app/keygen.py )
+    if [ -z "$WSID_PRIVATE_KEY" ]; then die "No private key generated!" ; fi
+else
+    say "WSID_PRIVATE_KEY contains predefined key, doing no regeneration"
 fi
 
 exec $*
